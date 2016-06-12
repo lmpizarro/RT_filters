55c193906b173e254eaac9f47aba8dbe1a9212a7
lmpizarro <lmpbici@gmail.com>
2016-06-12 20:09:39 -0300

    hpf init

--- include/filters.h
+++ include/filters.h
@@ -23,6 +23,22 @@ void LPF_6db_D(LPF_6db *lpf);
 
 typedef struct {
    float fc;
+   float a;
+   float sr;
+   float minp;
+   float mout;
+   float c0;
+   float c1;
+   float c2;
+
+}HPF_6db;
+
+HPF_6db *HPF_6db_C(const float fc, const float sr);
+float HPF_6db_R (HPF_6db *lp, float inp);
+void HPF_6db_D(HPF_6db *lpf);
+
+typedef struct {
+   float fc;
    float sr;
    LPF_6db * filter1;
    LPF_6db * filter2;
--- libs/filters.c
+++ libs/filters.c
@@ -39,6 +39,53 @@ void LPF_6db_D(LPF_6db *lpf){
   free(lpf);
 }
 
+/*
+ * HPF
+ */
+HPF_6db *HPF_6db_C(const float fc, const float sr)
+{
+  
+  float a0, a1, b0, b1, k;
+
+  HPF_6db *new = (HPF_6db *)calloc(1, sizeof(HPF_6db));
+  
+  new->fc = fc;
+  new->sr = sr;
+
+  k = 1.0f;
+  a0 = 2.0f * k;
+  a1 = a0; 
+
+  new->a = tan(PI * new->fc / new->sr);
+
+  b0 = 2.0f + new->a/new->sr;
+  b1 = -2.0f + new->a/new->sr;
+
+  new->c0 = a0 / b0 ;
+  new->c1 = a1 / b0;
+  new->c2 = b1/ b0;
+
+  new->minp = 0.0f;
+  new->mout = 0.0f;
+
+  return new;
+
+}
+
+float HPF_6db_R (HPF_6db *lp, float inp){
+    float out;
+
+    out = lp->c0 * inp + lp->c1 * lp->minp - lp->c2*lp->mout;
+    lp->mout = out;
+    lp->minp = inp;
+    return out;
+}
+
+void HPF_6db_D(HPF_6db *hpf){
+  free(hpf);
+}
+
+
 
 /*
 *   RMS filter
