459bb9eed883c51ebf1702db347ada79f1c1ec84
Luis Maria Pizarro <lmpizarro@gmail.com>
2016-06-15 16:12:21 -0300

    test filter 75hz hp

--- tests/test_filter.c
+++ tests/test_filter.c
@@ -111,6 +111,35 @@ int test_hp12db_filter (){
    Signal_D(s);
 }
 
+int test_hp12db_Sweep_filter (){
+   int i;
+   float sr, fc, out, damp;
+   HPF_12db_B * hpf1;
+   Signals * s;
+
+   sr = 44100.0f;
+
+   s = Signal_Sweep_Third_C(4.0f, sr); 
+   
+   fc = 75.0f; damp = 0.5f; 
+
+   hpf1 =  HPF_12db_B_C(fc, damp, sr);
+
+   //printf("a0 %f a1 %fi a2 %f\n", hpf1->a0, hpf1->a1, hpf1->a2);
+   //printf("b0 %f b1 %f b2 %f\n", hpf1->b0, hpf1->b1, hpf1->b2);
+   for (i =0; i < s->N; i ++) {
+    printf ("%f %f %f ", hpf1->minp, s->data[i], hpf1->mout);
+    out = HPF_12db_B_R (hpf1, s->data[i]);
+    printf ("%f\n",  out);
+   }
+
+   HPF_12db_B_D(hpf1);
+   Signal_D(s);
+}
+
+
+
+
 int test_lp12db_filter (){
    int N, i;
    float sr, fc, out, damp;
@@ -162,7 +191,7 @@ int test_sweep_lp12(){
   float out, q;
 
   q = 1.0;
-  s = Signal_Sweep_Third_C(4.0f, sr); 
+  s = Signal_Sweep_C(4.0f, sr); 
 
   lpf1 = LPF_12db_C(fc, q, sr);
 
@@ -213,7 +242,8 @@ int main (){
    //test_bp12db_filter();
    //test_lp12db_filter();
    //test_sweep_lp6();
-   test_sweep_lp12();
+   //test_sweep_lp12();
    //test_sweep_third();
+   test_hp12db_Sweep_filter();
    return (0);
 }
